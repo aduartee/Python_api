2ec36aefee16e232da72fa2e8c8a0968bb34e198
Arthur <arthurduartealves99@gmail.com>
2022-12-07 22:25:33 -0300

    Realizando as funçoes para cada URL

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ class ToDo(BaseModel):
     prazo : Optional[date]
 
 
-list = [
+lista = [
 
 ]
 
 
 @app.post('/inserir')
-def inserir(todo: Todo):
+def inserir(todo: ToDo):
     try:
         list.append(todo)
         return {'status': 'Sucesso!'}
@@ -25,6 +25,43 @@ def inserir(todo: Todo):
     except:
         return {'status': 'Erro ao inserir'}
 
+
+
+@app.post('/listar')
+def listar(opcao : int = 0):
+    if opcao == 0:
+        return lista
+    elif opcao == 1:
+        return list(filter(lambda x: x.realizada == False, lista))
+
+    elif opcao == 2:
+        return list(filter(lambda x: x.realizada == True, lista))
+
+
+@app.get('/listaunica/{id}')
+def listar(id : int):
+    try:
+        return lista[id]
+
+    except:
+        return {'status': 'Error'}
+
+@app.post('/alteraStatus')
+def altera(id : int):
+    try:
+        lista[id].realizada = not lista[id].realizada
+        return {'status' : 'Operação realizada' }
+    except:
+        return {'status' : 'Error'}
+
+@app.post('/Excluir')
+def excluir(id : int):
+    try:
+        del lista[id]
+        return {'status': 'Operação realizada'}
+
+    except:
+        return {'id' : 'Error'}
 
 
 
